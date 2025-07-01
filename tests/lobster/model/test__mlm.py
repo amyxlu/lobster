0749d1fa17bb48df112943521db8e892853a6024
Joseph Kleinhenz <kleinhej@gene.com>
2025-07-01 09:59:01 -0700

    add failing checkpoint test

--- tests/lobster/model/test__mlm.py
+++ tests/lobster/model/test__mlm.py
@@ -100,3 +100,23 @@ class TestLobsterPMLM:
     #     )
 
     #     assert model.config.hidden_size == 384
+
+
+def test_mlm_checkpoint(tmp_path):
+    print(f"{tmp_path=}")
+    model = LobsterPMLM("MLM_mini")
+
+    for k, v in model.named_parameters():
+        torch.nn.init.normal_(v)
+
+    model.save_pretrained(tmp_path / "checkpoint")
+
+    model2 = LobsterPMLM(str(tmp_path / "checkpoint"))
+
+    for (k1, v1), (k2, v2) in zip(model.named_parameters(), model2.named_parameters()):
+        assert k1 == k2
+        assert torch.equal(v1, v2)
+        assert not torch.equal(v2, torch.zeros_like(v2)), f"{k1=}, {k2=}"
+
+    assert torch.equal(model.model.lm_head.bias, model2.model.lm_head.bias)
+    assert torch.equal(model.model.lm_head.decoder.bias, model2.model.lm_head.decoder.bias)
